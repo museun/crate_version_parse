1b7a2df1791d8cd95e0762b37984d7d541ad9385
museun <museun@outlook.com>
2020-02-27 08:59:18 -0500

    fix feature, move method to type

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
 #![deny(
     missing_docs,
     missing_debug_implementations,
@@ -19,21 +19,21 @@
 
 ## Example
 ```rust
-# use crate_version_parse::{try_parse, CrateVersion};
+# use crate_version_parse::CrateVersion;
 let input = "zstd-sys-1.4.15+zstd.1.4.4";
-let crate_ = try_parse(input).unwrap();
+let crate_ = CrateVersion::try_parse(input).unwrap();
 assert_eq!(crate_.name, "zstd-sys");
-assert_eq!(crate_.ver, "1.4.15+zstd.1.4.4");
+assert_eq!(crate_.version, "1.4.15+zstd.1.4.4");
 
 let input = "wasi-0.9.0+wasi-snapshot-preview1";
-let crate_ = try_parse(input).unwrap();
+let crate_ = CrateVersion::try_parse(input).unwrap();
 assert_eq!(crate_.name, "wasi");
-assert_eq!(crate_.ver,  "0.9.0+wasi-snapshot-preview1");
+assert_eq!(crate_.version,  "0.9.0+wasi-snapshot-preview1");
 
 let input = "winapi-i686-pc-windows-gnu-0.4.0";
-let crate_ = try_parse(input).unwrap();
+let crate_ = CrateVersion::try_parse(input).unwrap();
 assert_eq!(crate_.name, "winapi-i686-pc-windows-gnu");
-assert_eq!(crate_.ver, "0.4.0");
+assert_eq!(crate_.version, "0.4.0");
 ```
 */
 
@@ -67,38 +67,6 @@ impl std::fmt::Display for Error {
 #[cfg(feature = "std")]
 impl std::error::Error for Error {}
 
-/// Tries to parse a crate `name` and `version` from a `$crate-$vers` format
-pub fn try_parse(input: &str) -> Result<CrateVersion<'_>, Error> {
-    let mut split = 0;
-    let mut iter = input
-        .chars()
-        .rev()
-        .map(|c| {
-            split += 1;
-            c
-        })
-        .peekable();
-
-    while let Some(c) = iter.next() {
-        match iter.peek() {
-            Some('-') if c.is_numeric() => break,
-            _ => {}
-        }
-    }
-
-    let midpoint = input.len() - split;
-
-    let name = input
-        .get(..midpoint)
-        .ok_or_else(|| Error::MissingName { pos: midpoint })?;
-
-    let ver = input
-        .get(midpoint + 1..)
-        .ok_or_else(|| Error::MissingVersion { pos: midpoint + 1 })?;
-
-    Ok(CrateVersion { name, ver })
-}
-
 /// A Crate + semver version
 #[derive(Debug, Copy, Clone, PartialEq, Hash, PartialOrd, Eq, Ord)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -106,7 +74,53 @@ pub struct CrateVersion<'a> {
     /// Name of the crate
     pub name: &'a str,
     /// Semver for the crate
-    pub ver: &'a str,
+    pub version: &'a str,
+}
+
+impl<'a> CrateVersion<'a> {
+    /// Tries to parse a crate `name` and `version` from a `$crate-$vers` format
+    ///
+    /// # Example
+    /// ```rust
+    /// # use crate_version_parse::CrateVersion;
+    /// let input = "wasi-0.9.0+wasi-snapshot-preview1";
+    ///
+    /// let version = CrateVersion::try_parse(input).unwrap();
+    /// assert_eq!(version, CrateVersion {
+    ///     name: "wasi",
+    ///     version: "0.9.0+wasi-snapshot-preview1",
+    /// });
+    /// ```
+    pub fn try_parse(input: &'a str) -> Result<Self, Error> {
+        let mut split = 0;
+        let mut iter = input
+            .chars()
+            .rev()
+            .map(|c| {
+                split += 1;
+                c
+            })
+            .peekable();
+
+        while let Some(c) = iter.next() {
+            match iter.peek() {
+                Some('-') if c.is_numeric() => break,
+                _ => {}
+            }
+        }
+
+        let midpoint = input.len() - split;
+
+        let name = input
+            .get(..midpoint)
+            .ok_or_else(|| Error::MissingName { pos: midpoint })?;
+
+        let version = input
+            .get(midpoint + 1..)
+            .ok_or_else(|| Error::MissingVersion { pos: midpoint + 1 })?;
+
+        Ok(CrateVersion { name, version })
+    }
 }
 
 #[cfg(test)]
@@ -119,41 +133,41 @@ mod tests {
                 "zstd-sys-1.4.15+zstd.1.4.4",
                 CrateVersion {
                     name: "zstd-sys",
-                    ver: "1.4.15+zstd.1.4.4",
+                    version: "1.4.15+zstd.1.4.4",
                 },
             ),
             (
                 "winapi-i686-pc-windows-gnu-0.4.0",
                 CrateVersion {
                     name: "winapi-i686-pc-windows-gnu",
-                    ver: "0.4.0",
+                    version: "0.4.0",
                 },
             ),
             (
                 "wasi-0.9.0+wasi-snapshot-preview1",
                 CrateVersion {
                     name: "wasi",
-                    ver: "0.9.0+wasi-snapshot-preview1",
+                    version: "0.9.0+wasi-snapshot-preview1",
                 },
             ),
             (
                 "ppv-lite86-0.2.5",
                 CrateVersion {
                     name: "ppv-lite86",
-                    ver: "0.2.5",
+                    version: "0.2.5",
                 },
             ),
             (
                 "log-0.4.8",
                 CrateVersion {
                     name: "log",
-                    ver: "0.4.8",
+                    version: "0.4.8",
                 },
             ),
         ];
 
         for (input, expected) in tests {
-            let crate_ = try_parse(input).unwrap();
+            let crate_ = CrateVersion::try_parse(input).unwrap();
             assert_eq!(crate_, *expected);
         }
     }
